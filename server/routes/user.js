ff2592d33cab627d5a29f87c95d62d4e45599bb0
M <m4mohaiman@gmail.com>
2024-01-09 01:28:15 +0600

    put update work

--- server/routes/user.js
+++ server/routes/user.js
@@ -1,5 +1,29 @@
-const router = require('express').Router();
+const User = require("../models/User");
+const { verifyToken , verifyTokenAndAuthorization } = require("./verifyToken");
 
+const router = require("express").Router();
 
+//upadte
+router.put("/:id", verifyTokenAndAuthorization, async (req, res) => {
+  if (req.body.password) {
+    req.body.password = CryptoJS.AES.encrypt(
+      req.body.password,
+      process.env.PASS_SEC
+    ).toString();
+  }
 
-module.exports = router
+  try {
+    const updatedUser = await User.findByIdAndUpdate(
+      req.params.id,
+      {
+        $set: req.body,
+      },
+      { new: true }
+    );
+    res.status(200).json(updatedUser);
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
+
+module.exports = router;
